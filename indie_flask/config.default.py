2e30ee6edaffe235debe434a3537d76d58318733
Darren Maczka <dmaczka@vt.edu>
2016-09-04 17:17:19 -0400

    rename config to config.default

--- indie_flask/config.default.py
+++ indie_flask/config.default.py
@@ -5,7 +5,7 @@ class Default(object):
     CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
     CELERY_TASK_SERIALIZER = 'json'
     CELERY_RESULT_SERIALIZER = 'json'
-    PUBLISH_ENDPOINT = 'http://localhost:5984/myblog/<id>'
+    PUBLISH_ENDPOINT = 'http://username:password@127.0.0.1:5984/myblog/{0}'
     TIMEZONE='US/Eastern'
     
 class Debug(Default):
